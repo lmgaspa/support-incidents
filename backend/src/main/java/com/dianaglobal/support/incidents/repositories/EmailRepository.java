3ffb0f8d2d1275b34e41fa730967aa93b5b4e0fc
luizmgaspa <luhmgasparetto@gmail.com>
2024-09-30 14:22:18 -0300

    microservices2add

--- backend/src/main/java/com/dianaglobal/support/incidents/repositories/EmailRepository.java
+++ backend/src/main/java/com/dianaglobal/support/incidents/repositories/EmailRepository.java
@@ -3,7 +3,5 @@ package com.dianaglobal.support.incidents.repositories;
 import com.dianaglobal.support.incidents.models.EmailModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.UUID;
-
 public interface EmailRepository extends MongoRepository<EmailModel, Long> {
 }
